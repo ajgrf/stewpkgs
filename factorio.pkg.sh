4cda60ed343d68643e2d7dd01deeb64cb541c513
Alex Griffin <a@ajgrf.com>
2018-05-12 17:01:08 -0500

    Add factorio

---
+++ factorio.pkg.sh
@@ -0,0 +1,8 @@
+name=factorio
+title="Factorio"
+version=0.16.36
+buildno=20079
+source=("gogdownloader://factorio/en3installer0>factorio_en_${version//./_}_${buildno}.sh")
+sum=(c4f61b621050d0ecdb99da25331ec17c96812084e083acac34f180ed04322cef)
+
+use gog
