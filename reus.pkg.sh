f0916c0d3c41ff2ef104ebd9a25bc2f36ae79c5c
Alex Griffin <a@ajgrf.com>
2018-05-12 17:17:53 -0500

    Add reus

---
+++ reus.pkg.sh
@@ -0,0 +1,7 @@
+name=reus
+title="Reus"
+version=2.0.0.2
+source=("gogdownloader://reus/en3installer2>gog_reus_${version}.sh")
+sum=(1d9d409366a747b2843160d3620d569c147e2cd05aba7a44d0970e0724f5976b)
+
+use gog
