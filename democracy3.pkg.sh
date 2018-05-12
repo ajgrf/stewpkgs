5f36de468396c45f3ee40a5fddbfe171f0cdb40f
Alex Griffin <a@ajgrf.com>
2018-05-12 18:16:29 -0500

    Add democracy3

---
+++ democracy3.pkg.sh
@@ -0,0 +1,7 @@
+name=democracy3
+title="Democracy 3"
+version=2.1.0.4
+source=("gogdownloader://democracy_3/en3installer2>gog_democracy_3_${version}.sh")
+sum=(3e62163e56b02e9b17fbd57558414e8b51a046a6e09e9c106864d39b20f7c02f)
+
+use gog
