2cd3da8fa6c4fbaa22b96ace0c4e67078ea0bd20
Alex Griffin <a@ajgrf.com>
2018-05-12 11:11:08 -0500

    Add shenzhen-io

---
+++ shenzhen-io.pkg.sh
@@ -0,0 +1,8 @@
+name=shenzhen-io
+title="SHENZHEN I/O"
+version=13.02
+buildno=18613
+source=("gogdownloader://shenzhen_io/en3installer0>shenzhen_io_en_${version//./_}_${buildno}.sh")
+sum=(cdecab5e814e8ffc759c4104d489bdd77f6a1f359f9e2d1ec9354f541f5cfbeb)
+
+use gog
