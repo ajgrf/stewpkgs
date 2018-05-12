056e4766528471ccbab2dfd3a8cb34cfd43edbd4
Alex Griffin <a@ajgrf.com>
2018-05-12 15:28:40 -0500

    Add human-resource-machine

---
+++ human-resource-machine.pkg.sh
@@ -0,0 +1,7 @@
+name=human-resource-machine
+title="Human Resource Machine"
+version=2.0.0.3
+source=("gogdownloader://human_resource_machine/en3installer2>gog_human_resource_machine_${version}.sh")
+sum=(ed34dd6fbf4242384dabf1f4690a4c1901e3a0c1267045fb9d0a73641b7b2169)
+
+use gog
