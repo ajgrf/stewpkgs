4097e0256449bb0d896700220362501d4fd05709
Alex Griffin <a@ajgrf.com>
2018-05-12 21:50:29 -0500

    ripgrep: Build from tarball

--- ripgrep.pkg.sh
+++ ripgrep.pkg.sh
@@ -1,7 +1,16 @@
 name=ripgrep
 version=0.8.1
 depends=(rust)
+aptdepends=(asciidoc)
+source=("https://github.com/BurntSushi/ripgrep/archive/${version}.tar.gz>rust-${version}.tar.gz")
+sum=(7035379fce0c1e32552e8ee528b92c3d01b8d3935ea31d26c51a73287be74bb3)
+
+build_phase() {
+	cargo build --release
+}
 
 install_phase() {
-	cargo install --root "$pkgdir" --vers "$version" ripgrep
+	mkdir -p "$pkgdir/bin" "$mandir/man1"
+	cp target/release/rg "$pkgdir/bin"
+	cp target/release/build/ripgrep-*/out/rg.1 "$mandir/man1"
 }
