dff68bc2cdec1d87fd0daf63daeda7bfad888d40
Alex Griffin <a@ajgrf.com>
2018-05-16 10:11:27 -0500

    Don't use configureflags

--- lowdown.pkg.sh
+++ lowdown.pkg.sh
@@ -3,6 +3,6 @@ version=0.3.2
 source=("https://kristaps.bsd.lv/lowdown/snapshots/lowdown-${version}.tar.gz")
 sum=(740cf6025c43ceaae9bcc1ffc3e94986aa62d65d91ef8173d444a1977c562094)
 
-preconfigure_phase() {
-	configureflags=(PREFIX="$pkgdir" MANDIR="$mandir")
+configure_phase() {
+	./configure PREFIX="$pkgdir" MANDIR="$mandir"
 }
--- skalibs.pkg.sh
+++ skalibs.pkg.sh
@@ -3,8 +3,8 @@ version=2.6.4.0
 source=("https://github.com/skarnet/skalibs/archive/v$version.tar.gz>$name-$version.tar.gz")
 sum=(842fac5df898434e8fb15ac03e0b97745eac689339bae1af62b8906d81f16107)
 
-preconfigure_phase() {
-	configureflags=("--prefix=$pkgdir" "--libdir=$pkgdir/lib"
-		"--includedir=$pkgdir/include"
-		"--sysdepdir=$pkgdir/lib/skalibs/sysdeps")
+configure_phase() {
+	./configure --prefix="$pkgdir" --libdir="$pkgdir/lib" \
+		--includedir="$pkgdir/include" \
+		--sysdepdir="$pkgdir/lib/skalibs/sysdeps"
 }
