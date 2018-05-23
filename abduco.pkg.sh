a4d0a987bd0832ee2a259fe8c9a57de7c51bd5c5
Alex Griffin <a@ajgrf.com>
2018-05-23 13:52:46 -0500

    Remove some unused packages

--- abduco.pkg.sh
+++
@@ -1,4 +0,0 @@
-name=abduco
-version=0.6
-source="http://www.brain-dump.org/projects/abduco/abduco-$version.tar.gz"
-sum=5dc3eaaa8d6ea54e22378468652dc9b4bb759641
--- khal.pkg.sh
+++
@@ -1,6 +0,0 @@
-name=khal
-version=0.9.8
-
-install_phase() {
-	pip3 install --system --prefix "$pkgdir" khal=="$version"
-}
