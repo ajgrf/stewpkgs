48b01f73e7e80470b9681d2c179903446ef7aa9a
Alex Griffin <a@ajgrf.com>
2018-06-07 18:34:41 -0500

    restic: Update to 0.9.0

--- restic.pkg.sh
+++ restic.pkg.sh
@@ -1,8 +1,8 @@
 name=restic
-version=0.8.3
+version=0.9.0
 description="backups done right"
 source="https://github.com/restic/restic/releases/download/v$version/restic-$version.tar.gz"
-sum=0cf697c88404b180d6d6ff2e7d2c27b2fcb9536da6dbdf15ad4d320af7e8f17c
+sum=5b46612254dcaec09a6f7ddae70e116f77c0f87ac7988dc379b34d0fd4bbc4c4
 
 use go
 
