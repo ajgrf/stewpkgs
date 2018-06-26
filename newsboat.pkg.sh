d79f65cb40f7c835a6ad5a0b06cd8b8c42dd7c42
Alex Griffin <a@ajgrf.com>
2018-06-26 10:22:48 -0500

    newsboat: Update to 2.12

--- newsboat.pkg.sh
+++ newsboat.pkg.sh
@@ -1,11 +1,11 @@
 name=newsboat
-version=2.11.1
+version=2.12
 description="text mode feed reader with podcast support"
 aptdepends="libncursesw5-dev libxml2-dev libstfl-dev libsqlite3-dev perl
 	pkg-config libcurl4-gnutls-dev libjson-c-dev asciidoc libxml2-utils
 	xsltproc docbook-xml docbook-xsl"
 source="https://newsboat.org/releases/$version/newsboat-$version.tar.xz"
-sum=aab67dcd386a3512812e1af0fddb8e1f4dce08d2a106946fe5ad405210ed37cf
+sum=9bbdbc2bca9e0c75a75588d89de3862f72e3fcb41c5d7db6210e2b491ffd43f4
 
 install_phase() {
 	autosudo make prefix="$pkgdir" install
