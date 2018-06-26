b767ac36579aab00d53e06cee299decfbd1d424a
Alex Griffin <a@ajgrf.com>
2018-06-26 13:00:39 -0500

    Add libgirara

---
+++ libgirara.pkg.sh
@@ -0,0 +1,6 @@
+name=libgirara
+version=0.3.0
+source="https://pwmt.org/projects/girara/download/girara-${version}.tar.xz"
+sum=b752fa5ff0a98cfa773bbcf66bb8a5e337a61cbb085d6577b3979178d17e41a2
+
+use meson
