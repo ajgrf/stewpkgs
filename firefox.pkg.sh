34ce1836ae16f90fb7fa1b6f331a8b9f39012f81
Alex Griffin <a@ajgrf.com>
2018-06-26 13:26:50 -0500

    firefox: Update to 60.1.0esr

--- firefox.pkg.sh
+++ firefox.pkg.sh
@@ -1,9 +1,9 @@
 name=firefox
-version=60.0.2
+version=60.1.0esr
 description="Mozilla Firefox web browser (binary)"
 source="https://download-installer.cdn.mozilla.net/pub/firefox/releases/$version/linux-x86_64/en-US/firefox-$version.tar.bz2
 	firefox.desktop"
-sum="90e5d4c106ae1756e6834593b357925f48a4e60c539e5f95f4bc323b5b7f4196
+sum="5b1420be9136a3a0ea01d4d9253bb65ecea8114d809222c4c2786a7ab077fd57
 	5bc512b747706793365da8ed51c9550804a7ee65e9073db84ac641149756c524"
 
 install_phase() {
