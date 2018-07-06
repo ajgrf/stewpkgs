cc13cd1775d9c4bac4224b5aee71ef7340913f0a
Alex Griffin <a@ajgrf.com>
2018-07-06 12:07:53 -0500

    anki: Updated to 2.0.52

--- anki.pkg.sh
+++ anki.pkg.sh
@@ -1,8 +1,8 @@
 name=anki
-version=2.0.50
+version=2.0.52
 description="powerful, intelligent flash cards (binary)"
 source="https://apps.ankiweb.net/downloads/current/anki-${version}-amd64.tar.bz2"
-sum=9a246a7598bc0332de64704cb75db51f27e6f941051f82b43e8cf168a22eba08
+sum=0304869bcc9e966839e3b3408048748be57d645ed0227fbaf0abebe8c18726d0
 
 install_phase() {
 	make PREFIX="$pkgdir" install
