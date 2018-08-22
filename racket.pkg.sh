43b5fd6f124ed7a0c1efaa159f5c3104e675dbb3
Alex Griffin <a@ajgrf.com>
2018-08-22 10:25:08 -0500

    racket: Update to 7.0

--- racket.pkg.sh
+++ racket.pkg.sh
@@ -1,10 +1,11 @@
 name=racket
-version=6.12
+version=7.0
+description="a programmable Scheme-like programming language"
 aptdepends="libffi-dev libfreetype6-dev libjpeg-dev libpango1.0-dev libpng-dev
 	libssl-dev libxaw7-dev xbitmaps libxft-dev libgl1-mesa-dev
 	libglu1-mesa-dev libx11-dev libcairo2-dev sqlite3"
 source="https://mirror.racket-lang.org/installers/$version/racket-$version-src.tgz"
-sum=655e052847c53e7c64ab9ad2247860c7b9abe2c4b0966474f0375127fff58c33
+sum=67bb624f5ae810b4ebf85e8f3acfaeda9c95d66e
 
 preconfigure_phase() {
 	mkdir src/build
