9b55739005d5d60d9c5d04ab295ef6f3d49538e6
Alex Griffin <a@ajgrf.com>
2018-08-22 11:30:18 -0500

    wee-slack: Update to 2.1.1

--- wee-slack.pkg.sh
+++ wee-slack.pkg.sh
@@ -1,9 +1,9 @@
 name=wee-slack
-version=2.0.0
+version=2.1.1
 description="a WeeChat plugin for Slack.com"
 aptdepends="python-websocket"
 source="https://github.com/wee-slack/wee-slack/archive/v$version.tar.gz>wee-slack-$version.tar.gz"
-sum=1090f667d9e52e977940f7d071fc88a4fd860bb9421dd0f703315886e4f57d67
+sum=8c0d5c1635b1e8e0440a7f27290ce3e3a1fd1ae9c682e0d898d2f2ba4dfd2f19
 
 install_phase() {
 	mkdir -p "$pkgdir/share/weechat/python"
