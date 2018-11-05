107a99071c4c63ab667391718ada3e3adfc7c3a3
Alex Griffin <a@ajgrf.com>
2018-11-04 18:47:20 -0600

    newsboat: Add gettext to aptdepends

--- newsboat.pkg.sh
+++ newsboat.pkg.sh
@@ -1,9 +1,9 @@
 name=newsboat
 version=2.12
 description="text mode feed reader with podcast support"
-aptdepends="libncursesw5-dev libxml2-dev libstfl-dev libsqlite3-dev perl
-	pkg-config libcurl4-gnutls-dev libjson-c-dev asciidoc libxml2-utils
-	xsltproc docbook-xml docbook-xsl"
+aptdepends="gettext libncursesw5-dev libxml2-dev libstfl-dev libsqlite3-dev
+	perl pkg-config libcurl4-gnutls-dev libjson-c-dev asciidoc
+	libxml2-utils xsltproc docbook-xml docbook-xsl"
 source="https://newsboat.org/releases/$version/newsboat-$version.tar.xz"
 sum=9bbdbc2bca9e0c75a75588d89de3862f72e3fcb41c5d7db6210e2b491ffd43f4
 
