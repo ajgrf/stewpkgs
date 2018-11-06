343b379c796c1454cf2efeaba7e36e99c3ea6fdd
Alex Griffin <a@ajgrf.com>
2018-11-06 12:53:12 -0600

    zathura-pdf-mupdf: Update to 0.3.4

--- zathura-pdf-mupdf.pkg.sh
+++ zathura-pdf-mupdf.pkg.sh
@@ -1,9 +1,9 @@
 name=zathura-pdf-mupdf
-version=0.3.3
+version=0.3.4
 depends="mupdf zathura"
 description="MuPDF plugin for zathura"
-source="https://pwmt.org/projects/zathura-pdf-mupdf/download/zathura-pdf-mupdf-${version}.tar.xz"
-sum=52b02ceee272a47f7ef9c2b73cc4f6fb480c02fdce0e7f6b7c8e93ad6ac46dfd
+source="https://github.com/pwmt/zathura-pdf-mupdf/archive/${version}.tar.gz>zathura-pdf-mupdf-${version}.tar.gz"
+sum=16417f3d259322ea7e6ec7106d519f5750f67079ff6b18fa879e75c1ded6f95e
 
 use meson
 
