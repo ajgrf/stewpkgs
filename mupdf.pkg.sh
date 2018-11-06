2ee6bd8c5bad729f20a10557cfd659f7f3dce36a
Alex Griffin <a@ajgrf.com>
2018-11-06 12:52:52 -0600

    mupdf: Update to 1.14.0

--- mupdf.pkg.sh
+++ mupdf.pkg.sh
@@ -1,10 +1,10 @@
 name=mupdf
-version=1.13.0
+version=1.14.0
 description="framework for PDF, XPS, and E-book documents"
 aptdepends="libfreetype6-dev pkg-config libjpeg-dev libjbig2dec0-dev
 	libopenjp2-7-dev zlib1g-dev"
-source="https://mupdf.com/downloads/mupdf-${version}-source.tar.xz"
-sum=7dd17ba51ae44e7e4614252aa5d3f3d615a26215
+source="https://mupdf.com/downloads/archive/mupdf-${version}-source.tar.xz"
+sum=b682828373fb1ff9911de08a2b2af7178eb78dc1
 
 build_phase() {
 	export CFLAGS="$CFLAGS -fPIC"
