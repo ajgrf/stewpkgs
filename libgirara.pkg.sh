bbf8f681cb3e188861cfc9d1c455eb4c8f182fd6
Alex Griffin <a@ajgrf.com>
2018-11-06 12:52:02 -0600

    libgirara: Update to 0.3.1

--- libgirara.pkg.sh
+++ libgirara.pkg.sh
@@ -1,6 +1,7 @@
 name=libgirara
-version=0.3.0
-source="https://pwmt.org/projects/girara/download/girara-${version}.tar.xz"
-sum=b752fa5ff0a98cfa773bbcf66bb8a5e337a61cbb085d6577b3979178d17e41a2
+version=0.3.1
+aptdepends="libgtk-3-dev"
+source="https://github.com/pwmt/girara/archive/${version}.tar.gz>girara-${version}.tar.gz"
+sum=7f6db861fee96e9b7062d007125bfb31771ab83d9541350d738b6439acb9b3e0
 
 use meson
