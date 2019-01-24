a9234d4ed8d6786149b0460df0b35d74e7d338bb
Alex Griffin <a@ajgrf.com>
2019-01-24 17:36:17 -0600

    go: Update to 1.11.5

--- go.pkg.sh
+++ go.pkg.sh
@@ -1,9 +1,9 @@
 name=go
-version=1.11.4
+version=1.11.5
 description="the Go programming language"
 depends="go1.4-bootstrap"
 source="https://storage.googleapis.com/golang/go$version.src.tar.gz"
-sum=4cfd42720a6b1e79a8024895fa6607b69972e8e32446df76d6ce79801bbadb15
+sum=bc1ef02bb1668835db1390a2e478dcbccb5dd16911691af9d75184bbe5aa943e
 
 build_phase() {
 	export GOROOT_BOOTSTRAP="$prefix/lib/go1.4-bootstrap"
