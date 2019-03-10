237e7ffbe2086a5632125b01e732491334c5442c
Alex Griffin <a@ajgrf.com>
2019-03-10 12:28:59 -0500

    maildir-deduplicate: Install from git

--- maildir-deduplicate.pkg.sh
+++ maildir-deduplicate.pkg.sh
@@ -1,8 +1,8 @@
 name=maildir-deduplicate
-version=2.1.0
+version=git
 description="deduplicate mails from maildir folders"
 aptdepends="python3-pip"
 
 install_phase() {
-	pip3 install --system --prefix "$pkgdir" "$name"=="$version"
+	pip3 install --system --prefix "$pkgdir" git+https://github.com/baip/maildir-deduplicate
 }
