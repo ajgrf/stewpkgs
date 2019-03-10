f480ff88b286b940348abcdfd340575f9e3053e9
Alex Griffin <a@ajgrf.com>
2019-03-10 13:38:22 -0500

    st: Update to 0.8.2

--- st.pkg.sh
+++ st.pkg.sh
@@ -1,13 +1,13 @@
 name=st
-version=0.8.1
+version=0.8.2
 description="a simple terminal implementation for X"
 aptdepends="libx11-dev libxft-dev x11proto-core-dev"
 source="http://dl.suckless.org/st/st-$version.tar.gz
-	https://st.suckless.org/patches/solarized/st-no_bold_colors-0.8.1.diff
-	https://st.suckless.org/patches/scrollback/st-scrollback-0.8.diff"
-sum="c4fb0fe2b8d2d3bd5e72763e80a8ae05b7d44dbac8f8e3bb18ef0161c7266926
-	5a6e2b745c7746228e0ee4e84214e3ac7054e6d451bc5843364e878bb2011e3b
-	8279d347c70bc9b36f450ba15e1fd9ff62eedf49ce9258c35d7f1cfe38cca226"
+	st-scrollback-0.8.2.diff
+	https://st.suckless.org/patches/solarized/st-no_bold_colors-0.8.1.diff"
+sum="aeb74e10aa11ed364e1bcc635a81a523119093e63befd2f231f8b0705b15bf35
+	33f0e43be7dc78db13c940fdf11a93475120761d97c0e8f5bcc37fdeacda1c42
+	5a6e2b745c7746228e0ee4e84214e3ac7054e6d451bc5843364e878bb2011e3b"
 
 use suckless
 
