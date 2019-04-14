72962e45496a8179a61369d05afba07659c1f5fe
Alex Griffin <a@ajgrf.com>
2019-04-14 15:48:49 -0500

    lowdown: Update to 0.4.3

--- lowdown.pkg.sh
+++ lowdown.pkg.sh
@@ -1,8 +1,8 @@
 name=lowdown
-version=0.4.2
+version=0.4.3
 description="simple Markdown translator"
 source="https://kristaps.bsd.lv/lowdown/snapshots/lowdown-${version}.tar.gz"
-sum=2a9d318a4f0898dd6cb65a46ad5cdf23d85660fcbbcebe19aa4828a4ca2f5c04
+sum=25d272edd47724e6892d4f401bc8404e892d5336a8c2cc42b166312d3865ddbe
 
 configure_phase() {
 	./configure PREFIX="$pkgdir" MANDIR="$mandir" LDFLAGS="-L$prefix/lib"
