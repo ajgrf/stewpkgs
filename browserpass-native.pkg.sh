41732cc44e06975b22441d87805baef5825debb8
Alex Griffin <a@ajgrf.com>
2019-04-16 11:21:04 -0500

    Add browserpass-native

---
+++ browserpass-native.pkg.sh
@@ -0,0 +1,14 @@
+name=browserpass-native
+version=3.0.6
+description="Host application for pass browser extension"
+depends="go"
+source="https://github.com/browserpass/browserpass-native/archive/$version.tar.gz>browserpass-native-$version.tar.gz"
+sum=6a2442192aad8fae3642b925d1ff04368eef3a390043e6164c8eb2fd2a438ceb
+
+postsetup_phase() {
+	unset LDFLAGS
+}
+
+configure_phase() {
+	make PREFIX="$pkgdir" configure
+}
