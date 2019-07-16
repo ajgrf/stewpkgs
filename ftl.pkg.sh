4a9b3734622c4000ad79cebdefaa759773d17efb
Alex Griffin <a@ajgrf.com>
2019-07-16 12:09:47 -0500

    ftl: Update to 1.6.9

--- ftl.pkg.sh
+++ ftl.pkg.sh
@@ -1,8 +1,8 @@
 name=ftl
 title="FTL: Advanced Edition"
-version=1.6.7
-buildno=18662
-source="gogdownloader://faster_than_light/en3installer0>ftl_advanced_edition_en_$(echo "$version" | sed 's/\./_/g')_$buildno.sh"
-sum=e20ea663c666a721f31304661e43c53609d1fef3aacbc4736a385dc3eaa4d06e
+version=1.6.9
+buildno=25330
+source="gogdownloader://faster_than_light/en3installer0>ftl_advanced_edition_$(echo "$version" | sed 's/\./_/g')_$buildno.sh"
+sum=a2675d3628fe6ea2692dee9d060d46acd65848849fc04da78b2aca65e7b3da0d
 
 use gog
