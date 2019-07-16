c8deedd5b759e133632a5f6a3d860f01285aa3a7
Alex Griffin <a@ajgrf.com>
2019-07-16 12:16:41 -0500

    kerbal-space-program: Update to 1.7.3.02594

--- kerbal-space-program.pkg.sh
+++ kerbal-space-program.pkg.sh
@@ -1,8 +1,8 @@
 name=kerbal-space-program
 title="Kerbal Space Program"
-version=1.4.3.2152
-buildno=20411
-source="gogdownloader://kerbal_space_program/en3installer0>kerbal_space_program_en_$(echo "$version" | sed 's/\./_/g')_$buildno.sh"
-sum=d12a2b4951b0687dd6600802480478a349c753202a7cce32462682cd909a85a8
+version=1.7.3.02594
+buildno=30887
+source="gogdownloader://kerbal_space_program/en3installer0>kerbal_space_program_$(echo "$version" | sed 's/\./_/g')_$buildno.sh"
+sum=3220bc6b8b163c90cc16df118200d73e5b5367638dcae59a53ae681bae45a0c2
 
 use gog
