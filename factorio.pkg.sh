e8ec871c016f9d55187c39c957b5180234ed6227
Alex Griffin <a@ajgrf.com>
2019-07-16 12:26:29 -0500

    factorio: Update to 0.17.41

--- factorio.pkg.sh
+++ factorio.pkg.sh
@@ -1,8 +1,8 @@
 name=factorio
 title="Factorio"
-version=0.16.36
-buildno=20079
-source="gogdownloader://factorio/en3installer0>factorio_en_$(echo "$version" | sed 's/\./_/g')_$buildno.sh"
-sum=c4f61b621050d0ecdb99da25331ec17c96812084e083acac34f180ed04322cef
+version=0.17.41
+buildno=29674
+source="gogdownloader://factorio/en3installer0>factorio_$(echo "$version" | sed 's/\./_/g')_$buildno.sh"
+sum=a57436ba035d90151162cc01dc0f33330a3b37d6553c974097597d08e67c8603
 
 use gog
