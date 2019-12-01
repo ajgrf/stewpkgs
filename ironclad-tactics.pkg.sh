214de92428d61b1df2a3017fa9374a866f331218
Alex Griffin <a@ajgrf.com>
2019-12-01 17:31:02 -0600

    Add ironclad-tactics

---
+++ ironclad-tactics.pkg.sh
@@ -0,0 +1,11 @@
+name=ironclad-tactics
+title="Ironclad Tactics"
+version=2.0.0.4
+aptdepends="libc6:i386 libasound2:i386 libasound2-data:i386
+	libasound2-plugins:i386 libcaca0:i386 libjpeg62-turbo:i386 libpng16-16:i386
+	libtiff5:i386 libncurses5:i386 libsqlite3-0:i386 libsdl1.2debian:i386
+	libsdl-mixer1.2:i386 libsdl-image1.2:i386 libgdiplus:i386"
+source="gogdownloader://ironclad_tactics_deluxe_edition/en3installer2>gog_ironclad_tactics_deluxe_edition_${version}.sh"
+sum=fa6d7e20c3dd22b0ccdbcbed7ef2d7cd447ab57a806a90c974b1340d8f665845
+
+use gog
