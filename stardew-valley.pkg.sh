35dfd719a05886d8ffdaa84f862b5358697dc0fe
Alex Griffin <a@ajgrf.com>
2019-12-02 15:30:43 -0600

    stardew-valley: Update to 1.4.362073756

--- stardew-valley.pkg.sh
+++ stardew-valley.pkg.sh
@@ -1,8 +1,8 @@
 name=stardew-valley
 title="Stardew Valley"
-version=1.3.36
-buildno=27827
+version=1.4.362073756
+buildno=34283
 source="gogdownloader://stardew_valley/en3installer0>stardew_valley_$(echo "$version" | sed 's/\./_/g')_${buildno}.sh"
-sum=af39d5f9d2c0c8be338af16f73bcd0dd1268f2af1cfe47f83f1f39b9cd0099e2
+sum=adfb7538d793a587ae3188b4f97c0508d14b14a1d12370ab2f765621fb64733d
 
 use gog
